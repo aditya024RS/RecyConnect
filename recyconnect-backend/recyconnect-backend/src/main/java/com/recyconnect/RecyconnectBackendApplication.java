1b5f98059050a5fdd615157b92090687b0144376
Aditya <s.adityaraj2204@gmail.com>
2025-08-05 15:18:26 +0530

    Add backend structure

---
+++ recyconnect-backend/recyconnect-backend/src/main/java/com/recyconnect/RecyconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recyconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyconnectBackendApplication.class, args);
+	}
+
+}
---
+++ recyconnect-backend/recyconnect-backend/src/test/java/com/recyconnect/RecyconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recyconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
